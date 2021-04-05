68a85575eb76402642827ccf19a70ff3937e1523
Suranjan <suranjanbiswas2002@gmail.com>
2021-04-05 22:00:37 +0530

    Created the apollo client and settled up the apollo-provider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import { ApolloClient } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client"; // helps to inject the data to the application recieved from the server
 import "./App.css";
+
 import BookList from "./components/BookList";
+
+//setting up the apollo client
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+});
 
 function App() {
   return (
-    <div id="main">
-      <h1>Bookworm</h1>
-      <BookList />
-    </div>
+    <ApolloProvider client={client}>
+      <div id="main">
+        <h1>Bookworm</h1>
+        <BookList />
+      </div>
+    </ApolloProvider>
   );
 }
 
