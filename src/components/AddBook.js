e86f7af9e7202993023f89cb2e2badd44b0b5e81
Suranjan <suranjanbiswas2002@gmail.com>
2021-04-06 15:23:20 +0530

    Added the basic logic for updating the form state and submitting data

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -4,6 +4,23 @@ import { graphql } from "@apollo/client/react/hoc";
 import { getAuthorsQuery } from "../utils/queries/queries";
 
 class AddBook extends Component {
+  state = {
+    bkname: "",
+    genre: "",
+    authorId: "",
+  };
+
+  changeValue = (name, value) => {
+    const stateObj = { ...this.state };
+    stateObj[name] = value;
+    this.setState({ ...stateObj });
+  };
+
+  submitForm = (event) => {
+    event.preventDefault();
+    console.log(this.state);
+  };
+
   authorOptions = () => {
     if (this.props.data.loading) {
       return;
@@ -14,25 +31,36 @@ class AddBook extends Component {
       </option>
     ));
   };
+
   render() {
     return (
       <form id="add-book">
         <div className="field">
           <label>Book Name</label>
-          <input type="text" />
+          <input
+            type="text"
+            value={this.state.bkname}
+            onChange={(e) => this.changeValue("bkname", e.target.value)}
+          />
         </div>
         <div className="field">
           <label>Genre</label>
-          <input type="text" />
+          <input
+            type="text"
+            value={this.state.genre}
+            onChange={(e) => this.changeValue("genre", e.target.value)}
+          />
         </div>
         <div className="field">
           <label>Author</label>
-          <select>
+          <select
+            onChange={(e) => this.changeValue("authorId", e.target.value)}
+          >
             <option>Select Authors</option>
             {this.authorOptions()}
           </select>
         </div>
-        <button>+</button>
+        <button onClick={(e) => this.submitForm(e)}>+</button>
       </form>
     );
   }
